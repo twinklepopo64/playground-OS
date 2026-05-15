263a9f444b6b682df1b9ca58f047dbc626d980bf
vombagel64 <Moodysnowman@outlook.com>
2026-05-14 20:05:11 -0400

    forgot to update todo list. also updated main.lua to be more organized and load in the new audio files.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# What is this???
+# What is Playground?
 Playground is planned to be a fake OS that lets you make your own apps, games, whatever you can think of, and actually run them inside the "OS"!
 
-DISCLAIMER: Progress is going to be VERY, VERY slow for now. I'm only a beginner, so I'm still figuring everything out.
+## DISCLAIMER: 
+Progress is going to be VERY, VERY slow for now. I'm only a beginner, so I'm still figuring everything out.
 Please be patient and stay tuned!!
+
+proper documentation will come with the first official release, but for now I'm addin as may comments as i can to explain everything
--- m_system/main.lua
+++ m_system/main.lua
@@ -1,12 +1,15 @@
 -- handles basic stuff, loads in assets, settings, sets up the window ad loads in other essential system files like the terminal and app loader
 
 function love.load()
-    -- controls the state of the program
+    -- state variables
     state = "startup"
-
     canEsc = true
-   -
+    
+    -- set up the window
+    love.window.setMode(800, 800, {resizable=true})
     love.window.setFullscreen(false)
+    love.window.setIcon(love.image.newImageData("m_assets/playIcon.png"))
+    love.window.setTitle("playground test window")
     
     love.graphics.newImage("m_assets/hand.png")
     local cr_data = love.image.newImageData("m_assets/hand.png")
@@ -16,8 +19,14 @@ function love.load()
     local defaultFont = love.graphics.newFont("fonts/W95F.otf", 16)
     love.graphics.setFont(defaultFont)
 
-    love.window.setIcon(love.image.newImageData("m_assets/playIcon.png"))
-    love.window.setTitle("playground test window")
+    -- load in audio files
+    love.audio.newSource("m_sfx/startsound.mp3", "static")
+    love.audio.newSource("m_sfx/openapp.mp3", "static")
+    love.audio.newSource("m_sfx/closeapp.mp3", "static")
+    love.audio.newSource("m_sfx/notif.mp3", "static")
+    love.audio.newSource("m_sfx/crashsound.mp3", "static")
+
+
 end
 
 function love.update(dt)
