fad19e42646a8f7a77f6a85ce2601f1c2bf86f6c
sodapop <Moodysnowman@outlook.com>
2026-06-22 10:19:01 -0400

    Organize sound loading into a sounds table
    Refactored sound loading into a table for better organization.

--- m_system/main.lua
+++ m_system/main.lua
@@ -1,9 +1,8 @@
 -- handles basic stuff, loads in assets, settings, sets up the window and loads in other essential system files like the terminal and app loader
-
-require("terminal")
-require("apploader")
-
 function love.load()
+    require("terminal")
+    require("apploader")
+    
     -- state variables
     state = "startup"
     canEsc = true
@@ -15,12 +14,14 @@ function love.load()
     local defaultFont = love.graphics.newFont("m_assets/fonts/W95F.otf", 23)
     love.graphics.setFont(defaultFont)
 
-    -- load in audio files
-    startSound = love.audio.newSource("m_sound/m_sfx/startup.mp3", "static")
-    openAppSound = love.audio.newSource("m_sound/m_sfx/openapp.mp3", "static")
-    closeAppSound = love.audio.newSource("m_sound/m_sfx/closeapp.mp3", "static")
-    notifSound = love.audio.newSource("m_sound/m_sfx/notif.mp3", "static")
-    crashSound = love.audio.newSource("m_sound/m_sfx/crashsound.mp3", "static")
+    -- set up sfx
+    sounds = {
+        startup = love.audio.newSource("m_sound/m_sfx/startup.mp3", "static")
+        openApp = love.audio.newSource("m_sound/m_sfx/openapp.mp3", "static")
+        closeApp = love.audio.newSource("m_sound/m_sfx/closeapp.mp3", "static")
+        notif = love.audio.newSource("m_sound/m_sfx/notif.mp3", "static")
+        crash = love.audio.newSource("m_sound/m_sfx/crashsound.mp3", "static")
+    }
 end
 
 function love.update(dt)
